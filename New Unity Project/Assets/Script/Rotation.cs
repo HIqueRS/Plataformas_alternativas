a38812a284713e0ff056a147ef654e48c44e0534
Hique <riquesouzalima11@gmail.com>
2020-11-11 22:37:03 -0300

    rolling fruits

---
+++ New Unity Project/Assets/Script/Rotation.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private Vector3 direction;
+    [SerializeField] private float vel;
+    [HideInInspector] public GameObject worm;
+
+    [HideInInspector]
+    public bool isLocked;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        isLocked = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementsInput();
+
+
+       
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(!isLocked)
+        {
+            rb.AddTorque(direction* vel);
+        }
+    }
+
+    private void MovementsInput()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+
+        if (!isLocked)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isLocked = true;
+
+                worm.SetActive(true);
+                worm.transform.SetParent(null);
+
+                
+            }
+
+
+        }
+    }
+
+    
+}
---
+++ New Unity Project/Assets/Script/WormMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormMovement : MonoBehaviour
+{
+    private Vector3 direction;
+    [SerializeField] private float vel;
+
+    private bool onFruit;
+    private GameObject fruit;
+    private Rotation fruitRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementsInput();
+
+
+        transform.LookAt(transform.position + direction * Time.deltaTime * vel);
+        transform.position += direction * Time.deltaTime * vel;
+
+    }
+
+    private void MovementsInput()
+    {
+        direction.z = Input.GetAxis("Horizontal");
+        direction.x = -1 * Input.GetAxis("Vertical");
+
+        if(onFruit)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                transform.parent = fruit.transform;
+                fruitRotation.isLocked = false;
+                fruitRotation.worm = this.gameObject;
+
+                this.gameObject.SetActive(false);
+            }
+           
+           
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Fruit")
+        {
+            fruit = other.gameObject;
+            fruitRotation = fruit.GetComponent<Rotation>();
+            onFruit = true;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Fruit")
+        {
+            onFruit = false;
+            fruit = null;
+        }
+    }
+}
